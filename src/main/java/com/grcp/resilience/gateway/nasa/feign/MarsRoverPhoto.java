a217f65f11296ee26f2c08939b7d11f39c862303
Gabriel Pereira <gabrielrcpereira@gmail.com>
2020-12-08 15:28:50 -0300

    updating fallback methods

--- src/main/java/com/grcp/resilience/gateway/nasa/feign/MarsRoverPhoto.java
+++ src/main/java/com/grcp/resilience/gateway/nasa/feign/MarsRoverPhoto.java
@@ -11,16 +11,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @FeignClient(value = "${api.external.nasa.name}", url = "${api.external.nasa.source}")
 public interface MarsRoverPhoto {
 
-    @CircuitBreaker(name = "nasapi")
-    @Retry(name = "nasapi", fallbackMethod = "findSpacePhotosFallback")
+    @CircuitBreaker(name = "nasapi", fallbackMethod = "findSpacePhotosCircuitBreakerFallback")
+    @Retry(name = "nasapi", fallbackMethod = "findSpacePhotosFeignFallback")
     @GetMapping("/mars-photos/api/v1/rovers/curiosity/photos?sol=10&api_key=${api.external.nasa.key}")
     MarsRoverResponse findSpacePhotos();
 
-    default MarsRoverResponse findSpacePhotosFallback(FeignException e) {
+    default MarsRoverResponse findSpacePhotosFeignFallback(FeignException e) {
         return MarsRoverResponse.builder().isError(true).fallbackSource("retry").build();
     }
 
-    default MarsRoverResponse findSpacePhotosFallback(CallNotPermittedException e) {
+    default MarsRoverResponse findSpacePhotosCircuitBreakerFallback(CallNotPermittedException e) {
         return MarsRoverResponse.builder().isError(true).fallbackSource("circuitBreaker").build();
     }
 }
