250280c22ed52840b95a3fbaaaab78abbbff7075
Gabriel Pereira <gabrielrcpereira@gmail.com>
2020-12-09 06:42:45 -0300

    adding challenge test

--- src/main/java/com/grcp/resilience/gateway/dojo/feign/MockedApiClient.java
+++ src/main/java/com/grcp/resilience/gateway/dojo/feign/MockedApiClient.java
@@ -1,12 +1,26 @@
 package com.grcp.resilience.gateway.dojo.feign;
 
 import com.grcp.resilience.gateway.dojo.feign.json.MockedApiResponse;
+import feign.FeignException;
+import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @FeignClient(value = "${api.external.dojo.name}", url = "${api.external.dojo.source}")
 public interface MockedApiClient {
 
-    @GetMapping(value = "/test-success", consumes = "application/json")
+    @CircuitBreaker(name = "nasapi", fallbackMethod = "requestSuccessApiCircuitBreakerFallback")
+    @Retry(name = "nasapi", fallbackMethod = "requestSuccessApiFeignFallback")
+    @GetMapping(value = "/test-success-after-seven-times", consumes = "application/json")
     MockedApiResponse requestSuccessApi();
+
+    default MockedApiResponse requestSuccessApiFeignFallback(FeignException e) {
+        return MockedApiResponse.builder().fallbackSource("retry").build();
+    }
+
+    default MockedApiResponse requestSuccessApiCircuitBreakerFallback(CallNotPermittedException e) {
+        return MockedApiResponse.builder().fallbackSource("circuitBreaker").build();
+    }
 }
